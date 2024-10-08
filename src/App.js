04666572c79c938b7ee995e42ba1146892019a80
Juan Esteban Mazuera Yunda 2043008 <juan.yunda@correounivalle.edu.co>
2024-10-08 16:45:34 -0500

    Arreglos a Sigue-la-secuencia | Juego terminado sin detalles. Pruebas de pixijs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './styles/index.css';
 import Home from './pages/Home';
 import Dashboard from './modules/Dashboard/Dashboard';
+import Pixi from './modules/Games/pixijs/index.js';
 import Game1 from './modules/Games/matriz-de-memoria/Matriz-de-memoria';
 import Game2 from './modules/Games/sigue-la-secuencia/Sigue-la-secuencia';
 import Game3 from './modules/Games/sopa-de-letras/Sopa-de-letras';
@@ -15,6 +16,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/pixi" element={<Pixi />} /> {/* Usamos PixiApp aquí */}
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/games/matriz-de-memoria" element={<Game1 />} />
           <Route path="/games/sigue-la-secuencia" element={<Game2 />} />
